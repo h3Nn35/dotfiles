c54396f8f9a8073ac437fcff55548be3c56daa5c
Christian Hennes <78818014+h3Nn35@users.noreply.github.com>
2023-10-13 13:08:17 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,6 +9,14 @@
 # by Christian Hennes (2023) 
 # ----------------------------------------------------- 
 
+is_command() {
+    # Checks to see if the given command (passed as a string argument) exists on the system.
+    # The function returns 0 (success) if the command exists, and 1 if it doesn't.
+    local check_command="$1"
+
+    command -v "${check_command}" >/dev/null 2>&1
+}
+
 show_logo(){
     echo -e "
              _       _    __ _ _           
