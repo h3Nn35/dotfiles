9bdcce41f1d6df71196e48b50cd021adeec33085
Christian Hennes <iphone.henne@gmail.com>
2024-07-28 10:45:06 +0200

    Text

--- nvim/.config/nvim/lua/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -7,6 +7,13 @@ return {
       "MunifTanjim/nui.nvim",
     },
     config = function()
-      vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>')  
+      vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>')
+      require("neo-tree").setup({
+        filesystem = {
+          filtered_items = {
+            visible = true, -- this will show hidden files
+          },
+        },
+      })
     end
 }
--- nvim/.config/nvim/lua/vim-options.lua
+++ nvim/.config/nvim/lua/vim-options.lua
@@ -2,4 +2,6 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+vim.cmd("set nu rnu") 		-- Absolute und relative Zeilennummern an
+vim.cmd("set cursorline")	-- Aktuelle Linie unterstreichen
 vim.g.mapleader = " "
