ec59634ddce22cd9dce4a83e8908659dbbb834a2
Christian Hennes <h3nn35@pm.me>
2024-07-29 15:09:32 +0200

    No commit text

--- installyay/installyay.sh
+++ installyay/installyay.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
-#if [ ! command -v yay &> /dev/null ]; then
-  cd /opt
-  sudo git clone https://aur.archlinux.org/yay.git
-  sudo chown -R christian ./yay
-  cd yay
-  makepkg -si
-#else
-#  echo "yay ist bereits installiert"
-#fi
+cd /opt
+sudo git clone https://aur.archlinux.org/yay.git
+sudo chown -R $USER ./yay
+cd yay
+makepkg -si
