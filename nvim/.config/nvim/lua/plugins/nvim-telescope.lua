19930e3eb608042cdee4b85b9465a31e162b7845
Christian Hennes <iphone.henne@gmail.com>
2024-07-31 15:33:00 +0200

    No commit text

--- nvim/.config/nvim/lua/plugins/nvim-telescope.lua
+++ nvim/.config/nvim/lua/plugins/nvim-telescope.lua
@@ -3,6 +3,58 @@ return {
     tag = '0.1.8',
     dependencies = { 'nvim-lua/plenary.nvim' },
     config = function()
+
+      local telescope = require('telescope')
+      telescope.setup{
+        defaults = {
+          vimgrep_arguments = {
+            'rg',
+            '--color=never',
+            '--no-heading',
+            '--with-filename',
+            '--line-number',
+            '--column',
+            '--smart-case',
+            '--hidden'
+          },
+          prompt_prefix = "> ",
+          selection_caret = "> ",
+          entry_prefix = "  ",
+          initial_mode = "insert",
+          selection_strategy = "reset",
+          sorting_strategy = "descending",
+          layout_strategy = "horizontal",
+          layout_config = {
+            horizontal = {
+              mirror = false,
+            },
+            vertical = {
+              mirror = false,
+            },
+          },
+          file_sorter = require'telescope.sorters'.get_fuzzy_file,
+          file_ignore_patterns = {},
+          generic_sorter = require'telescope.sorters'.get_generic_fuzzy_sorter,
+          path_display = {},
+          winblend = 0,
+          border = {},
+          borderchars = { '─', '│', '─', '│', '┌', '┐', '┘', '└' },
+          color_devicons = true,
+          use_less = true,
+          set_env = { ['COLORTERM'] = 'truecolor' },
+          file_previewer = require'telescope.previewers'.vim_buffer_cat.new,
+          grep_previewer = require'telescope.previewers'.vim_buffer_vimgrep.new,
+          qflist_previewer = require'telescope.previewers'.vim_buffer_qflist.new,
+          -- Developer configurations: Not meant for general override
+          buffer_previewer_maker = require'telescope.previewers'.buffer_previewer_maker
+        },
+        pickers = {
+          find_files = {
+            hidden = true,
+          }
+        }
+      }
+
       local builtin = require("telescope.builtin")
       vim.keymap.set('n', '<C-p>', builtin.find_files, {})
       vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
